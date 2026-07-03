ca4eb0ca0958f9a5fce40b7d3770b054d358076f
code-a13 <adityakrishnamoorthi13@gmail.com>
2026-07-03 08:58:58 +0530

    July 2nd Task

--- Bitwise/Task.java
+++ Bitwise/Task.java
@@ -1,5 +1,16 @@
 public class Task {
     public static void main(String[] args) {
-        //
+        // 1.Multiply a Number by 2
+        System.out.println(5 << 1);
+        // 2.multiply a Number by 4
+        System.out.println(8<<2);
+        // 3.multiply a Number by 8
+        System.out.println(7<<3);
+        // 4.divide a Number by 2
+        System.out.println(18>>1);
+        // 5.divide a Number by 4
+        System.out.println(36>>2);
+        // 6.divide a Number by 8
+        System.out.println(80>>3);
     }
 }
